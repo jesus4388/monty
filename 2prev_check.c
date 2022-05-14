c29ecf2e27f766bbd8ed83f509c71f5129df9d00
root <jesus4388>
2022-05-14 07:36:22 -0700

    add

--- 2prev_check.c
+++ 2prev_check.c
@@ -15,16 +15,18 @@ int check_sum(stack_t **head, unsigned int line)
 		for (i = 0; aux != NULL; i++)
 			aux = aux->next;
 		if (i > 1)
+		{
 			sum_node(head, line);
+		}
 		else
 		{
-			fprintf(stderr, "L%d: can't add, stack too", line);
+			fprintf(stderr, "L%d: can't add, stack too\n", line);
 			return (-1);
 		}
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't add, stack too", line);
+		fprintf(stderr, "L%d: can't add, stack too\n", line);
 		return (-1);
 	}
 	return (0);
